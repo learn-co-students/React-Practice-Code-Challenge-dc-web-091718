b951c5aa77d8686b1ecf145cc9087e9884b31c1b
github username <github email address>
2018-11-28 14:08:15 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,50 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      sushis: [],
+      eatenSushi: [],
+      budget: 150,
+      counter: 0
+    }
+  }
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/sushis')
+    .then(res => res.json())
+    .then(data => this.setState({
+      sushis: data
+    }))
+  }
+
+  handleDisplaySushi = () => {
+    return this.state.sushis.slice(this.state.counter, this.state.counter+4)
+  }
+
+  increaseCounter = () => {
+    this.setState({
+      counter: this.state.counter + 4
+    })
+  }
+
+  handleEaten = (sushi) => {
+    if(this.state.budget >= sushi.price) {
+      this.setState({
+        eatenSushi: [...this.state.eatenSushi, sushi],
+        budget: this.state.budget - sushi.price
+      })
+    }
+  }
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer increaseCounter={this.increaseCounter} handleDisplaySushi={this.handleDisplaySushi()} sushis={this.state.sushis} handleEaten={this.handleEaten} eaten={this.state.eatenSushi}/>
+        <Table budget={this.state.budget} eaten={this.state.eatenSushi}/>
       </div>
     );
   }
